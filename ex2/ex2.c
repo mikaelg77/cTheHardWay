74b78ab383122ff9d5fb96356a478a8468450142
Mikael Gustafsson <m77@localhost.localdomain>
2017-09-22 21:58:38 +0200

    First version of ex2

---
+++ ex2/ex2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	int age = 40;
+	int height = 169;
+
+	printf("I am %d years old and I am %d centimeters tall.\n", age, height);
+
+	return 0;
+}
