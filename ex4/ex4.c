703d21c7cb9d26293b9984a0ff7724db31ec7495
Mikael Gustafsson <m77@localhost.localdomain>
2017-09-22 22:14:20 +0200

    Corrected version of ex4

---
+++ ex4/ex4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	int age = 40;
+	int height = 169;
+
+	printf("I am %d years old.\n", age);
+	printf("I am %d centimeters tall.\n",height);
+
+	return 0;
+}
